1a41b13dcc8e2d6f46423a860d63cf6267d2c857
tanvi meer <tanvimeer.tops@gmail.com>
2024-08-17 09:21:21 +0530

    chnage doen

--- src/dummy/fummy.java
+++ src/dummy/fummy.java
@@ -3,5 +3,9 @@ package dummy;
 public class fummy {
 public static void main(String[] args) {
 	System.out.println("this is dummy project");
+	printSometing(args);
+}
+public static void printSometing(String[] args) {
+	System.out.println("this is print project");
 }
 }
