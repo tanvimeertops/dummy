b1c00660ea6e81528dceb8721e24614cc03b50f2
tanvi meer <tanvimeer.tops@gmail.com>
2024-08-20 15:56:58 +0530

    changes from feature

--- src/dummy/fummy.java
+++ src/dummy/fummy.java
@@ -2,14 +2,14 @@ package dummy;
 
 public class fummy {
 public static void main(String[] args) {
-	System.out.println("this is dummy project"
+	System.out.println("this is fummy project"
 			+ "and i know that");
 	printSometing(args);
 }
 public static void printSometing(String[] args) {
-	System.out.println("this is print project");
+	System.out.println("Please print project");
 }
 public static void doSometing(String[] args) {
-	System.out.println("this is print project");
+	System.out.println("Please do something");
 }
 }
