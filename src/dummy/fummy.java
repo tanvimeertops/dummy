fe1f7c82a8853d773fea96377eef5982c4c13efa
tanvi meer <tanvimeer.tops@gmail.com>
2024-08-21 16:59:46 +0530

    21 aug 5pm

--- src/dummy/fummy.java
+++ src/dummy/fummy.java
@@ -3,11 +3,11 @@ package dummy;
 public class fummy {
 public static void main(String[] args) {
 	System.out.println("this is fummy project"
-			+ "and i know that");
+			+ "and i know that!!!");
 	printSometing(args);
 }
 public static void printSometing(String[] args) {
-	System.out.println("Please print project");
+	System.out.println("$$$Please print project");
 }
 public static void doSometing(String[] args) {
 	System.out.println("Please do something");
