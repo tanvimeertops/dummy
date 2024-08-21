5a3772b83c00336a54e1ad35a5f94952d8494944
tanvi meer <tanvimeer.tops@gmail.com>
2024-08-21 17:20:52 +0530

    change 4

--- src/dummy/fummy.java
+++ src/dummy/fummy.java
@@ -5,7 +5,9 @@ public static void main(String[] args) {
 	System.out.println("this is fummy project"
 			+ "and i know that");
 }
-
+public static void printSometing(String[] args) {
+	System.out.println("Please print something");
+}
 public static void doSometing(String[] args) {
 	System.out.println("Please do something");
 }
