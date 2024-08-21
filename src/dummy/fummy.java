a5d8355779a707ca2f87b1870430a067c476dab5
tanvi meer <tanvimeer.tops@gmail.com>
2024-08-21 17:11:49 +0530

    change 3

--- src/dummy/fummy.java
+++ src/dummy/fummy.java
@@ -3,12 +3,9 @@ package dummy;
 public class fummy {
 public static void main(String[] args) {
 	System.out.println("this is fummy project"
-			+ "and i know that!!!");
-	printSometing(args);
+			+ "and i know that");
 }
-public static void printSometing(String[] args) {
-	System.out.println("$$$Please print project");
-}
+
 public static void doSometing(String[] args) {
 	System.out.println("Please do something");
 }
