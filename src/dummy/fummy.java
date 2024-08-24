bde48dd253a900b31ff1671234e0c49114b768b0
tanvi meer <tanvimeer.tops@gmail.com>
2024-08-24 15:41:06 +0530

    123

--- src/dummy/fummy.java
+++ src/dummy/fummy.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("this is dummmy project"
 			+ "and i know that!!!");
 }
-public static void printSometing(String[] args) {
+public static void printSome(String[] args) {
 	System.out.println("Please print something");
 }
 public static void doSometing(String[] args) {
